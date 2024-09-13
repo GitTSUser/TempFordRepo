3482101f099e87e4e4e44bd02664e8eedfc21236
GitTSUser <kiranmadhan@gmail.com>
2024-09-13 11:21:36 +0530

    added new method in FindFact class

--- FindFact.java
+++ FindFact.java
@@ -1,5 +1,13 @@
 public class FindFact{
 
+public static int findFactorialV1(int n){
+	int result=1;
+	for(int i=1;i<=n;i++){
+	result=result*i;
+	}
+
+return result;
+}
 public static int findFactorial(int n){
 
 	if(n==1){
